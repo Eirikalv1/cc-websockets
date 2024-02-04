6cdc30a5ebb63c7644b439b76693a06e177b95b2
Eirik <eirikalv@icloud.com>
2024-02-04 21:11:42 +0100

    message protocol and real time block updates

--- client/src/sockets.rs
+++ client/src/sockets.rs
@@ -30,51 +30,59 @@ impl Sockets {
                 }
                 Event::Message(_, msg_frame) => {
                     if let Message::Text(msg) = msg_frame.clone() {
-                        if msg == "nil" {
-                            log::info!("Command executed successfully");
-                            
-                        } else if msg.chars().next() == Some('[') {
-                            let names = msg
-                                .split("][")
-                                .next()
-                                .unwrap()
-                                .trim_start_matches('[')
-                                .trim_end_matches(']')
-                                .split(',')
-                                .map(|s| s.trim_matches('"').to_string())
-                                .collect::<Vec<String>>();
-                            let coords_linearized = msg
-                                .split("][")
-                                .nth(1)
-                                .unwrap()
-                                .trim_start_matches('[')
-                                .trim_end_matches(']')
-                                .split(',')
-                                .map(|s| s.parse::<u16>().unwrap())
-                                .collect::<Vec<u16>>();
-
-                            assert!(
-                                blocks.len() == coords_linearized.len(),
-                                "turtle and client have different scan radiuses"
-                            );
-
-                            for (block_index, name_index) in coords_linearized.iter().enumerate() {
-                                let block = &mut blocks[block_index];
-
-                                block.coord = Block::delinearize(block_index as u16);
-                                if *name_index > 0 {
-                                    block.name = names[*name_index as usize - 1].to_owned();
-                                } else {
-                                    block.name = "minecraft:air".to_string();
-                                }
-                            }
-                        } else {
-                            log::error!("Turtle does not understand the command!");
-                        }
+                        Self::message_event(&msg, blocks);
                     }
                 }
             }
         };
+    }
+
+    fn message_event(msg: &str, blocks: &mut [Block]) {
+        // Message protocol: "[Message type integer][Data]" e.g. '0minecraft:stone'
+        // '0' -> Command executed successfully
+        // '1[Error message]' -> Commaand failed
+        // 2[Comma separated blocks][Block index] -> Geo Scanner data
+
+        if msg.chars().next() == Some('0') {
+            log::info!("Command executed successfully");
+        } else if msg.chars().next() == Some('1') {
+            log::error!("Turtle does not understand the command! {}", &msg[1..]);
+        } else if msg.chars().next() == Some('2') {
+            let names = msg
+                .split("][")
+                .next()
+                .unwrap()
+                .trim_start_matches('[')
+                .trim_end_matches(']')
+                .split(',')
+                .map(|s| s.trim_matches('"').to_string())
+                .collect::<Vec<String>>();
+            let coords_linearized = msg
+                .split("][")
+                .nth(1)
+                .unwrap()
+                .trim_start_matches('[')
+                .trim_end_matches(']')
+                .split(',')
+                .map(|s| s.parse::<u16>().unwrap())
+                .collect::<Vec<u16>>();
+
+            assert!(
+                blocks.len() == coords_linearized.len(),
+                "turtle and client have different scan radiuses"
+            );
+
+            for (block_index, name_index) in coords_linearized.iter().enumerate() {
+                let block = &mut blocks[block_index];
+
+                block.coord = Block::delinearize(block_index as u16);
+                if *name_index > 0 {
+                    block.name = names[*name_index as usize - 1].to_owned();
+                } else {
+                    block.name = "minecraft:air".to_string();
+                }
+            }
+        }
     }
 }
 
