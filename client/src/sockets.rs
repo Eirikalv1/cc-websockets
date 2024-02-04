b889398524b7399819bc3ddea4b7251e8b33693c
Eirik <eirikalv@icloud.com>
2024-02-04 17:01:15 +0100

    Graceful handling of different types of packets

--- client/src/sockets.rs
+++ client/src/sockets.rs
@@ -29,41 +29,47 @@ impl Sockets {
                     self.client = None;
                 }
                 Event::Message(_, msg_frame) => {
-                    if let Message::Text(mut msg) = msg_frame.clone() {
+                    if let Message::Text(msg) = msg_frame.clone() {
                         if msg == "nil" {
-                            msg = "Command executed successfully".to_string();
-                        }
+                            log::info!("Command executed successfully");
+                            
+                        } else if msg.chars().next() == Some('[') {
+                            let names = msg
+                                .split("][")
+                                .next()
+                                .unwrap()
+                                .trim_start_matches('[')
+                                .trim_end_matches(']')
+                                .split(',')
+                                .map(|s| s.trim_matches('"').to_string())
+                                .collect::<Vec<String>>();
+                            let coords_linearized = msg
+                                .split("][")
+                                .nth(1)
+                                .unwrap()
+                                .trim_start_matches('[')
+                                .trim_end_matches(']')
+                                .split(',')
+                                .map(|s| s.parse::<u16>().unwrap())
+                                .collect::<Vec<u16>>();
 
-                        let names = msg
-                            .split("][")
-                            .next()
-                            .unwrap()
-                            .trim_start_matches('[')
-                            .trim_end_matches(']')
-                            .split(',')
-                            .map(|s| s.trim_matches('"').to_string())
-                            .collect::<Vec<String>>();
-                        let coords_linearized = msg
-                            .split("][")
-                            .nth(1)
-                            .unwrap()
-                            .trim_start_matches('[')
-                            .trim_end_matches(']')
-                            .split(',')
-                            .map(|s| s.parse::<u16>().unwrap())
-                            .collect::<Vec<u16>>();
+                            assert!(
+                                blocks.len() == coords_linearized.len(),
+                                "turtle and client have different scan radiuses"
+                            );
 
-                        assert!(blocks.len() == coords_linearized.len(), "turtle and client have different radiuses");
+                            for (block_index, name_index) in coords_linearized.iter().enumerate() {
+                                let block = &mut blocks[block_index];
 
-                        for (block_index, name_index) in coords_linearized.iter().enumerate() {
-                            let block = &mut blocks[block_index];
-
-                            block.coord = Block::delinearize(block_index as u16);
-                            if *name_index > 0 {
-                                block.name = names[*name_index as usize - 1].to_owned();
-                            } else {
-                                block.name = "minecraft:air".to_string();
+                                block.coord = Block::delinearize(block_index as u16);
+                                if *name_index > 0 {
+                                    block.name = names[*name_index as usize - 1].to_owned();
+                                } else {
+                                    block.name = "minecraft:air".to_string();
+                                }
                             }
+                        } else {
+                            log::error!("Turtle does not understand the command!");
                         }
                     }
                 }
